78138dc453465bdb85b511edba46c5d9697206e1
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-07 18:10:27 +0100

    adding base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,9 +8,9 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request) {
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles("ui/html/home.page.tmpl")
 
 	if err != nil {
 		log.Println(err.Error())
@@ -29,7 +29,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -37,7 +37,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func CreateSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", 405)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/create", CreateSnippet)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
