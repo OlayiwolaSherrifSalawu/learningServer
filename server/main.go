31c29fd88e642ba0cebb279f0dc334bb18ed5a6b
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-07 12:19:20 +0100

    learnt the application method of using the server

--- server/main.go
+++ server/main.go
@@ -10,75 +10,74 @@ import (
 )
 
 type Application struct {
-	store *Store
+	store    *Store
+	fileName string
 }
 type Task struct {
 	Id          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
-	IsCompleted bool   `json:"isCompleted"`
+	IsComplete  bool   `json:"isComplete"`
 }
 
 type Store map[int]Task
 
-func (app *Application) handleTask(w http.ResponseWriter, r *http.Request) {
-task:= app.store[]
-}
-
-func (s *Store) readJson(fileName string) error {
+func (s *Store) readJson(fileName string) ([]byte, error) {
 	file, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+func (s *Store) unmarsha(slices []byte, toMarshal *[]Task) error {
+
+	err := json.Unmarshal(slices, toMarshal)
 	if err != nil {
 		return err
 	}
-
-	err = json.Unmarshal(file, s)
-	if err != nil {
-		return err
+	for i, val := range *toMarshal {
+		(*s)[i] = val
 	}
 	return nil
 }
-func (s *Store) getJson(id int, fileName string) ([]byte, error) {
-	err := s.readJson(fileName)
-	if err != nil {
-		return nil, err
-	}
-
-	val, ok := (*s)[id]
-	if !ok {
-		return nil, fmt.Errorf("task cant be found ")
-	}
-	byts, err := json.Marshal(val)
-	if err != nil {
-		return nil, err
-	}
-	return byts, nil
-}
-func task(w http.ResponseWriter, r *http.Request) {
-
+func (app *Application) handleTask(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.Header().Set("Allow", http.MethodGet)
 		w.WriteHeader(405)
 		w.Write([]byte("method not allowed"))
 		return
 	}
-	// if r.URL.Path != "/" {
-	// 	http.NotFound(w, r)
-	// 	return
-	// }
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-
-	// data, err := json.Marshal(file
-	// w.Write(file)
+	task := (*app.store)[id]
+	byts, err := json.Marshal(task)
+	if err != nil {
+		w.Header().Set("Allow", http.MethodGet)
+		w.WriteHeader(501)
+		w.Write([]byte("error reading files "))
+		return
+	}
+	w.Write(byts)
 }
 
 func main() {
+	var temp *[]Task
+	theStore := &Store{}
+	fileByte, err := theStore.readJson("result.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	theStore.unmarsha(fileByte, temp)
+	app := &Application{
+		store: theStore,
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", task)
-	log.Println("started server at port 8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.handleTask)
+	log.Println("server started at port 8080")
+	errs := http.ListenAndServe(":8080", mux)
+	log.Fatal(errs)
 }
