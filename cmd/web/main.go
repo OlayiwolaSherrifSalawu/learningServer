1eab90203f68d0706e2717289f0f17e3222a97b9
olayiwola sherrif salawu <salawuolayiwola181@gmail.com>
2026-04-12 08:14:47 +0100

    added loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Addr       string
+	StaticAddr string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticAddr, "static", "ui/static/", "Static files")
+
+	infoLogger := log.New(os.Stdout, "INFO \t", log.Ldate|log.Ltime)
+	errors := log.New(os.Stderr, "ERROR \t", log.Ldate|log.Ltime)
+	flag.Parse()
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticAddr))
 
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/create", CreateSnippet)
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe("localhost:4000", mux)
-	log.Fatal(err)
+
+	serve := &http.Server{
+		Addr:     cfg.Addr,
+		Handler:  mux,
+		ErrorLog: errors,
+	}
+	infoLogger.Printf("Starting server on %s", cfg.Addr)
+	err := serve.ListenAndServe()
+	serve.ErrorLog.Fatal(err)
+
 }
