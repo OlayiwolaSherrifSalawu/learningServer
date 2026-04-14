11ca249d1fea15a359a12d1d8004a9667a9dbccc
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-14 18:43:23 +0100

    added database connections

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
 	Addr       string
 	StaticAddr string
 }
-
 type Application struct {
 	ErrorLoger *log.Logger
 	InfoLogger *log.Logger
@@ -19,13 +23,32 @@ func main() {
 	cfg := new(Config)
 	app := new(Application)
 	mux, cfg := app.routes(cfg)
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MY SQL DSN")
+	flag.Parse()
+	db, err := openDB(*dsn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 	serve := &http.Server{
 		Addr:     cfg.Addr,
 		Handler:  mux,
 		ErrorLog: app.ErrorLoger,
 	}
 	app.InfoLogger.Printf("Starting server on %s", cfg.Addr)
-	err := serve.ListenAndServe()
+	err = serve.ListenAndServe()
 	serve.ErrorLog.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
