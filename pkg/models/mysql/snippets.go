849d34da20476d3455fb68cc0a29fe7af9747b92
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-23 17:35:31 +0100

    fixed database

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -10,8 +10,18 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (s *SnippetModel) Insert(title, content, created, expires string) (int, error) {
-	return 0, nil
+func (s *SnippetModel) Insert(title, content,  expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title,content,created,expires) VALUES(?,?, UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(), INTERVAL?DAY))`
+
+	result, err := s.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 func (s *SnippetModel) Get(ID int) (*models.Snippet, error) {
