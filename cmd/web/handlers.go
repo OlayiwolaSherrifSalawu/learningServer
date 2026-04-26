61069132256e2f17f7b5156b7e14d409fc16f138
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-26 17:37:42 +0100

    added: template cache

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 	"strconv"
 
 	"alexedwards.net/snippetbox/pkg/models"
@@ -93,4 +94,31 @@ func (app *Application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 	// w.Write([]byte("Create a new snippet..."))
+}
+
+func cacheTemplate(dir string) (map[string]*template.Template, error) {
+	cache := make(map[string]*template.Template)
+
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partail.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
