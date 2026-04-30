ee3d77393f73256f737fc798e71f43a8d70a94d5
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-04-30 23:45:16 +0100

    added alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/justinas/alice"
 )
 
 func (app *Application) routes(cfg *Config) (http.Handler, *Config) {
+	standardMid := alice.New(app.recoverOnPanic, app.logRequest, secureHeaders)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP Address")
 	app.ErrorLoger = log.New(os.Stderr, "ERROR \t", log.Ldate|log.Ltime)
 	app.InfoLogger = log.New(os.Stdout, "INFO \t", log.Ldate|log.Ltime)
@@ -17,5 +20,5 @@ func (app *Application) routes(cfg *Config) (http.Handler, *Config) {
 	mux.HandleFunc("/snippet", app.ShowSnippet)
 	mux.HandleFunc("/snippet/create", app.CreateSnippet)
 	mux.Handle("/ui/static/", http.StripPrefix("/ui/static/", fileServer))
-	return app.recoverOnPanic(app.logRequest(secureHeaders(mux))), cfg
+	return standardMid.Then(mux), cfg
 }
