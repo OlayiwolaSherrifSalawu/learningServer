6fb5af3b08c29a6c71711a6d4bc549f91c8f936e
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-05-02 00:15:50 +0100

    added errors handles

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"alexedwards.net/snippetbox/pkg/models"
 )
@@ -46,6 +48,7 @@ func (app *Application) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 func (app *Application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 4096)
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
@@ -55,6 +58,33 @@ func (app *Application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+	// Initialize a map to hold any validation errors.
+	errors := make(map[string]string)
+	// Check that the title field is not blank and is not more than 100 characters
+	// long. If it fails either of those checks, add a message to the errors// long. If it fails either of those checks, add a message to the errors
+	// map using the field name as the key.
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+	// Check that the Content field isn't blank.
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+	// Check the expires field isn't blank and matches one of the permitted
+	// values ("1", "7" or "365").
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+	// If there are any errors, dump them in a plain text HTTP response and return
+	// from the handler.
+	if len(errors) > 0 {
+		fmt.Fprint(w, errors)
+		return
+	}
 
 	id, err := app.snippet.Insert(title, content, expires)
 	if err != nil {
