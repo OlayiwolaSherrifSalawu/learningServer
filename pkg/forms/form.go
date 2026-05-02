fef0b7f694d9f3b063c8355c017b3460b02f5975
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-05-02 23:31:53 +0100

    added max length error

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -1,15 +1,40 @@
 package forms
 
-import "net/url"
+import (
+	"net/url"
+	"strings"
+)
 
 type Form struct {
-	url   url.Values
+	url.Values
 	Error errors
 }
 
 func NewForm(data url.Values) *Form {
 	return &Form{
-		url:   data,
-		Error: errors(map[string][]string{}),
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, val := range fields {
+		value := f.Get(val)
+		if value == "" {
+			return
+		}
+		if strings.TrimSpace(value) == "" {
+			f.Error.Add(value, "This Field is required it cant be empty")
+		}
+	}
+}
+
+func (f *Form) MaxLength(field string, d int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if len(value) > d {
+		f.Error.Add(value, "content is too long.")
 	}
 }
